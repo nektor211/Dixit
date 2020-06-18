6f6cc969330d9243199dfeefa85801faed6e6ae0
lolieatapple <lolieatapple@gmail.com>
2020-06-19 00:34:34 +0800

    update game rules.

--- imports/ui/About.jsx
+++ imports/ui/About.jsx
@@ -10,17 +10,17 @@ class About extends Component {
               <div className="About">
                 <h2>
                   <span id="badge" className="badge badge-pill badge-dark">
-                   About
+                   说明
                   </span>
                 </h2>
-                <h5> Dixit is a card game using a deck of cards illustrated with dreamlike images, 
-                     players select cards that match a title suggestsed by the "storyteller", and 
-                     attempt to vote which card the "storyteller" selected...</h5> 
+                <h5> Dixit中文名“只言片语”是一种纸牌游戏，使用一副配有梦幻图像的纸牌，
+                    玩家选择与“故事讲述者”建议的标题相匹配的卡片
+                    尝试为“故事讲述者”选择的卡片投票…</h5> 
               </div>
               <div className="Game SetUP">
                 <h2>
                   <span id="badge" className="badge badge-pill badge-dark">
-                   Players
+                   玩家人数
                   </span>
                 </h2>
                 <h5>
@@ -30,28 +30,28 @@ class About extends Component {
               <div className="Rules">
                 <h2>
                   <span id="badge" className="badge badge-pill badge-dark">
-                   Rules
+                   游戏规则
                   </span>
                 </h2>
                 <h5>
                   <ul>
-                    <li>Story teller pick a card and give it a title</li>
-                    <li>Players pick a card which best matches story teller's title to put in POOL</li>
-                    <li>Players vote for cards in POOL, try best to guess story teller's card</li>
-                    <li>Each player will become "story teller" once in a game</li>
+                    <li>讲述人选择一张卡牌并给出一段描述</li>
+                    <li>其他玩家从手牌中选择与描述最贴近的一张牌，放入牌池</li>
+                    <li>玩家在牌池中投票，尽量猜出哪一张是讲述人的牌</li>
+                    <li>每一个玩家轮流成为讲述人</li>
                   </ul>
                 </h5>
               </div>
               <div className="Scores">
                 <h2>
                   <span id="badge" className="badge badge-pill badge-dark">
-                   Scores
+                   得分
                   </span>
                 </h2>
                 <h5>
                   <ul>
-                    <li>Players +1 point upon voting the right card</li>
-                    <li>Story teller +3 points if some player but not all players vote for his/her card</li>
+                    <li>其他玩家猜中讲述人的卡牌时，加1分</li>
+                    <li>当有人猜中讲述人的卡牌，但不是全部全猜中时，讲述人加3分</li>
                   </ul>
                 </h5>
               </div>
